47560990b3c425d75e80511fe3760df3e26df288
manvendra09 <ankursingh297@gmail.com>
2024-04-24 14:06:30 +0530

    Updates

---
+++ precache-manifest.c61736cbe737ba7c0d58719f6214d39c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e76e8d82fb58e04bf159b4973d67b70",
+    "url": "/Weather_app/index.html"
+  },
+  {
+    "revision": "8f3a094b1f3dae8b04dc",
+    "url": "/Weather_app/static/css/main.e62ae5f9.chunk.css"
+  },
+  {
+    "revision": "001b67d05a335e326bff",
+    "url": "/Weather_app/static/js/2.6a7fe4e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather_app/static/js/2.6a7fe4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f3a094b1f3dae8b04dc",
+    "url": "/Weather_app/static/js/main.3db7ca14.chunk.js"
+  },
+  {
+    "revision": "9c8ebb4815cb10e2902a",
+    "url": "/Weather_app/static/js/runtime-main.836b0f69.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/Weather_app/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "50dc3d97c484b1c4172f30cd54459142",
+    "url": "/Weather_app/static/media/background.50dc3d97.jpg"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/Weather_app/static/media/city.e1698332.jpg"
+  }
+]);
